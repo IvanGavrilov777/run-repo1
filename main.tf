48158202ba26cb6b7799cd3ad36697eecaa42786
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2024-07-12 10:54:05 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 module "pet_ivancho" {
-  source  = "app.terraform.io/healthy-organ/pet/random//vancho/azsum/submodulche"
+  source  = "app.terraform.io/healthy-organ/pet/random//pet"
   version = "4.0.0"
 }
